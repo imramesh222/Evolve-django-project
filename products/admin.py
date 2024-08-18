e7e7eb78fdaa4d7d15365136b50765dd54de5497
imramesh222 <imrameshrawat@gmail.com>
2024-08-18 17:04:34 +0545

    rendering products image to html file

--- products/admin.py
+++ products/admin.py
@@ -5,3 +5,4 @@ from . models import *
 admin.site.register(Setting)
 admin.site.register(Product)
 admin.site.register(P_product)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -13,7 +13,15 @@ class Setting(models.Model):
   def __str__(self):
     return self.title
 
+class Category(models.Model):
+  name=models.CharField(max_length=200,null=True)
+  description=models.TextField(max_length=1000,blank=True)
+  created_at=models.DateTimeField(auto_now_add=True,null=True)
+
+  def __str__(self):
+    return self.name
 class Product(models.Model):
+  category=models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
   name=models.CharField(max_length=200)
   image=models.CharField(max_length=1000)
   price=models.DecimalField(max_digits=10,decimal_places=2)
@@ -24,6 +32,7 @@ class Product(models.Model):
 
   def __str__(self):
     return self.name
+  
   
 class P_product(models.Model):
   name=models.CharField(max_length=200)
